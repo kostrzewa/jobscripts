c1b374639c5f9a439d1d2467d37af6d19015f06e
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2012-11-07 12:38:00 +0100

    make comparison independent of relative file content

--- generators/highstat/utils/checkruntime.sh
+++ generators/highstat/utils/checkruntime.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
+# this utility reads the "runtimes.csv" output ${1} of the highstat R routine
+# and compares the runtimes to the reference specified in REFTFILE
+
+# the REFSAMPLES and SAMPLES variables hold a list of reference samples
+# and samples in the two runtimes files
+
+# the names in these two variables are immaterial, they only serve to label to columns
+# in the files. They must have the same number and they must be in the same order
+# for the tool to work correctly. The order must also match the column order in the two
+# tables that are read. The row order in the tables is of no consequence.
+
+# currently the comparison of partial runtime output is not supported because 
+# of the aforementioned importance of ordering
+
 REFTFILE="../runtimes_csw.csv"
 
-REFSAMPLES="refhmc0 refhmc1 refhmc2 refhmc3 refhmc_ndclover refhmc_nosplit_ndclover refhmc_nocsw_ndclover \
-         refhmc_nosplit_nocsw_ndclover refhmc_cloverdet refhmc_tmcloverdet refhmc_check_ndclover_tmcloverdet\
-         refhmc_check_ndclover_nocsw_tmcloverdet refhmc_tmcloverdetratio"
+TREFSAMPLES=`head -n1 ${REFTFILE}`
 
-SAMPLES="hmc0 hmc1 hmc2 hmc3 hmc_ndclover hmc_nosplit_ndclover hmc_nocsw_ndclover \
-         hmc_nosplit_nocsw_ndclover hmc_cloverdet hmc_tmcloverdet hmc_check_ndclover_tmcloverdet\
-         hmc_check_ndclover_nocsw_tmcloverdet hmc_tmcloverdetratio"
+# prepend "ref" to each name in TREFSAMPLES
+for i in $TREFSAMPLES; do
+  REFSAMPLES="${REFSAMPLES} ref${i}"
+done
+
+SAMPLES=`head -n1 ${1}`
 
 # skip the headers in the files
 readrefheaderflag=0
@@ -29,14 +44,17 @@ while read refname ${REFSAMPLES}; do
 
     if [[ $refname = $name ]]; then
       for i in ${SAMPLES}; do
-        j="ref"${i}
-        if [[ -z ${!i} ]]; then
+        if [[ -z ${!i} || ${!i} = "NA" ]]; then
           echo "The $name run of sample $i has an empty time measurement. The set is probably not complete!"
           continue
         fi
-        if [[ `echo "scale=6;out=0;r=${!i}/${!j};if(r>1.05) out=1;out" | bc` -eq 1 ]]; then
-          echo "The $name run of sample $i took longer than the reference time: ${!i} > ${!j} !"
-        fi
+        for j in ${REFSAMPLES}; do
+          if [[ ${j} = "ref"${i} ]]; then
+            if [[ `echo "scale=6;out=0;r=${!i}/${!j};if(r>1.05) out=1;out" | bc` -eq 1 ]]; then
+              echo "The $name run of sample $i took longer than the reference time: ${!i} > ${!j} !"
+            fi
+          fi
+        done
       done
     fi
   done < ${1}
