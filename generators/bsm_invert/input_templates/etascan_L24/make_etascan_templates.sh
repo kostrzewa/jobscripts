4c7b7904d185217eca704a06cd91caa76cedfe37
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-05-13 14:01:48 +0200

    bsm_invert: add template_template file as well as a script to generate input templates with different etas

---
+++ generators/bsm_invert/input_templates/etascan_L24/make_etascan_templates.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "${1}" -o -z "${2}" -o -z "${3}" ]; then
+  echo "one of the specified parameters was an empty string"
+  echo "usage: ./make_etascan_templates.sh <target basename> <template_template.input> '<eta values as space separated string enclosed in single quotes>'"
+  echo "pay special attention to the single quotes around the list of eta values!"
+  exit 1
+fi
+
+for eta in ${3}; do
+  # replace minus signs in the eta value with an "m"
+  texteta=$( echo $eta | sed "s/-/m/g" )
+  targetfile=${1}.${texteta}.input
+  cp ${2} ${targetfile}
+  sed -i "s/ETA/${eta}/g" ${targetfile}
+done
