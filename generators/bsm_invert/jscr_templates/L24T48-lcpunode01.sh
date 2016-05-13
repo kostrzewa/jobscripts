2baf46ceebc00c70d42bbef472cf2e54ff7c8e80
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-05-13 15:03:51 +0200

    bsm_invert: disable IO checks in L24 eta scan on B85.24 and provide the generated input templates

---
+++ generators/bsm_invert/jscr_templates/L24T48-lcpunode01.sh
@@ -0,0 +1,18 @@
+#PBS -N NAME
+#PBS -l walltime=48:00:00,mem=15gb
+#PBS -M bartosz.kostrzewa@desy.de
+#PBS -l nodes=lcpunode01:ppn=32
+#PBS -j oe
+
+efile=EXEC
+
+export OMP_NUM_THREADS=32
+
+cd JDIR
+
+date > NAME.log
+
+KMP_AFFINITY=compact ${efile} -v | tee -a NAME.log
+
+date >> NAME.log
+
