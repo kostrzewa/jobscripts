57ae03d57ed99337b2b93a9cd7b936d2e2613840
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-05-23 12:08:24 +0200

    updated script to extract iteration count and true residual from etascan output files

--- utils/bsm_etascan_iteroutgrep.sh
+++ utils/bsm_etascan_iteroutgrep.sh
@@ -7,22 +7,24 @@ etas="0 1 m1.1 m1.15 m1.2 m2.2 m2.3 m2.4"
   
 
 ofile=iters_${base}.dat
-echo "kappa eta conf iters" > ${ofile}
+echo "kappa eta conf iters res" > ${ofile}
 for kappa in ${kappas}; do
   for conf in ${confs}; do
     for eta in ${etas}; do
       valeta=$( echo $eta | sed 's/m/-/g' )
       dirname=$( printf "%s_%s_Kap%s" $base $eta $kappa )
       filename=$( printf "%s/%s/%s_%s.log" $dirname $conf $dirname $conf )
-      iters=$( grep "Inversion done" ${filename} | grep iterations | awk '{print $5}' )
+      # this produces a bash array
+      iters=( $( grep "Inversion done" ${filename} | grep iterations | awk '{print $5 }' ) )
+      # the true residual is decorated with an exclamation mark...
+      residues=( $( grep "Inversion done" ${filename} | grep iterations | awk '{print $10 }' | sed 's/!//g' ) )
       if [ $DEBUG -eq 1 ]; then
         echo dirname = $dirname
         echo filename = $filename
-        echo iters = $iters
         echo
       fi
-      for iter in $iters; do
-        echo $kappa $valeta $conf $iter >> ${ofile}
+      for i in $( seq 0 $(( ${#iters[@]} - 1 )) ); do
+        echo $kappa $valeta $conf ${iters[i]} ${residues[i]}  >> ${ofile}
       done
     done
   done
