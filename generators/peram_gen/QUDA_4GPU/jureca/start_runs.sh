e2bd27e0dc8b74b41f424025e7288648c40a9fef
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-09-05 11:34:39 +0200

    updated peram_gen/QUDA_4GPU job script generator for jureca gpu partition

--- generators/peram_gen/QUDA_4GPU/jureca/start_runs.sh
+++ generators/peram_gen/QUDA_4GPU/jureca/start_runs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-conf_start=24
-conf_end=104
+conf_start=2000
+conf_end=2200
 conf_step=8
 
 rnd_start=0
-#rnd_end=0
 rnd_end=5
 
 for i in $(seq ${conf_start} ${conf_step} ${conf_end}); do
@@ -14,7 +13,7 @@ for i in $(seq ${conf_start} ${conf_step} ${conf_end}); do
   cd cnfg${j}
   for rnd in $(seq ${rnd_start} ${rnd_end}); do
     rnd2=$( printf %02d $rnd )
-    qsub job_script_${j}_${rnd2}.pbs
+    sbatch job.slurm.${j}_${rnd2}.cmd
   done
   cd ..
   
--- generators/peram_gen/QUDA_4GPU/jureca/templates/job.slurm.cmd
+++ generators/peram_gen/QUDA_4GPU/jureca/templates/job.slurm.cmd
@@ -1,13 +1,26 @@
-#PBS -N jobname
-#PBS -l walltime=10:00:00,mem=52gb
-#PBS -l nodes=1:ppn=8:gpus=4:exclusive_process
-#PBS -M bartosz.kostrzewa@desy.de
-#PBS -m abe
-#PBS -j oe
+#!/bin/bash -x
+#SBATCH --job-name=JOBNAME                                                                                                                                                                                     
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bartosz.kostrzewa@desy.de
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=12
+#SBATCH --output=outputs/out.JOBNAME.%j
+#SBATCH --error=outputs/out.JOBNAME.%j
+#SBATCH --time=06:00:00
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:4
 
-exe=/hadron/bartek/code/peram_gen_QUDA_4GPU/main/main
+module restore nvidia
 
-cd blahblah
-mpirun.openmpi -x OMP_NUM_THREADS=2 -np 4 -npersocket 2 \
-${exe} -LapHsin ./INFILE &> ./OUTFILE
-chmod 664 *
+rundir=RUNDIR
+exe=EXEC
+outfile=OUTFILE
+infile=INFILE
+
+cd ${rundir}
+date > ${outfile}
+QUDA_RESOURCE_PATH=${HOME}/misc/jureca/quda_resources/develop-serial OMP_NUM_THREADS=12 srun ${exe} -LapHsin ${infile} | tee -a ${outfile}
+date >> ${outfile}
+
+wait
